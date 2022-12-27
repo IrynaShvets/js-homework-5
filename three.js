cfee4abf29b97314f6d8ef10ea41d3668f10f6d7
IrynaShvets <86491334+IrynaShvets@users.noreply.github.com>
2022-12-27 14:07:35 +0200

    added changes three

--- three.js
+++ three.js
@@ -1,15 +1,22 @@
-const number = prompt("Введіть, будь ласка число.");
+const num = prompt("Введіть, будь ласка число.");
 
-if (typeof parseInt(number) ===  "number") {
-    alert("Дякую, за вашу відповідь.");
-  } 
+isEven(num);
 
-if (number === "" || typeof +number !==  "number") {
-    alert("Введіть число.");
-    prompt("Введіть, будь ласка число.");
+function isEven(num) {
+  const numParse = Number.isFinite(parseFloat(num));
+
+  if (numParse === false) {
+    alert("Ви ввели не число.");
+  } else if (numParse === true) {
+    if (parseInt(num) % 2 == 0) {
+      alert("Дякую, за вашу відповідь, ваше число парне.");
+    }
+    if (Math.abs(parseInt(num) % 2) == 1) {
+      alert("Дякую, за вашу відповідь, ваше число не парне.");
+    }
   }
+}
 
-  
 /* 
 3. С помощью prompt попросите пользователя ввести число. 
 Сделать проверку на пустую строку и если значение является НЕ числом.
